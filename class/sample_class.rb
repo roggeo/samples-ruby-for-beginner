22895ceea6181479b53a651dde68e96c9baefd3a
Geovani <geovanirog@gmail.com>
2018-11-30 07:32:32 -0200

    ruby class

---
+++ class/sample_class.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+class SampleClass
+	def initialize (message)
+		@message = message
+	end
+	def hello
+		puts @message.to_s
+	end
+end
+
+sample_class = SampleClass.new('Hello my friend!')
+sample_class.hello
