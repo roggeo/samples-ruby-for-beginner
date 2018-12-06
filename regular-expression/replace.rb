2cbc2b2742b4070d849d588a7727a3b6cd649317
unknown <roggeo>
2018-12-06 14:04:29 -0200

    regular expression

---
+++ regular-expression/replace.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+phone = "2004-959-559 #This is Phone Number"
+
+# Delete Ruby-style comments
+phone = phone.sub!(/#.*$/, "")   
+puts "Phone Num : #{phone}"
+
+# Remove anything other than digits
+phone = phone.gsub!(/\D/, "")    
+puts "Phone Num : #{phone}"
